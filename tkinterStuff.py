763ec73014d6bdfc12423f8a95263b8cdfa5e9ea
chancep1214 <137582264+chancep1214@users.noreply.github.com>
2026-01-09 06:55:08 -0500

    Add files via upload

--- tkinterStuff.py
+++ tkinterStuff.py
@@ -1,6 +1,6 @@
 import random
-import emoji
 import tkinter as tk
+import math
 
 def rectangleClick(event):
     # Coords of click
@@ -14,25 +14,44 @@ def rectangleClick(event):
         if (checkRectangleBomb(rectID)):
             clickedBomb()
         else:
-            changeRectangle(rectID)
-        
+            # Retrieves center coordinates from clicked rectangle
+            center = getCenterOfRectangle(rectID)
+            #centerX1, centerY1 = center[0], center[1]
+            # Stores the number of bombs in tiles vicinity
+            bombCounter = getNumBombVicinity(center)
+            changeRectangle(rectID, center, bombCounter)
 
+def getNumBombVicinity(clickedCenter):
+    numBombVicinity = 0
+    # Can edit this for loop eventually to clear all connected blocks with no bombs
+    for bomb in bombLocations:
+        bombCoords = centerCoordsList[bomb - 1]
+        # Calculates distance between clicked rectangle center and 
+        distance = math.dist(clickedCenter, bombCoords)
+        if (distance <= 50*math.sqrt(2)):
+            numBombVicinity += 1
+    return numBombVicinity
+
+def getCenterOfRectangle(rectID):
+    index = rectID - 1
+    return centerCoordsList[index]
+    
 def checkRectangleBomb(rectID):
     if (rectID in bombLocations):
         return True
     else:
         return False
 
-def changeRectangle(rectID):
-    canvas.itemconfig(rectID, fill="green")
+def changeRectangle(rectID, center, bombCounter):
+    canvas.itemconfig(rectID, fill="yellow")
+    canvas.create_text
 
 def clickedBomb():
     endText = "Oh no, you clicked a bomb. Game Over!"
     canvas.create_text(690,360, text=endText, font=("Arial", 40))
+    canvas.unbind("<Button-1>")
+
     
-
-
-
 root = tk.Tk()
 root.title("Minesweeper")
 root.geometry("1280x720")
@@ -51,6 +70,8 @@ columns = 24
 # Creates empty board that store 0's
 board = [[0 for row in range(columns)] for col in range(rows)]
 
+coordsList = []
+centerCoordsList = []
 # Outer for loop creates 12 rows while inner loop creates 24 columns of boxes
 for i in range(rows):
     for j in range(columns):
@@ -58,6 +79,9 @@ for i in range(rows):
         id = canvas.create_rectangle(topX,topY,botX,botY,outline="black", fill="white")
         # Stores id in the board
         board[i][j] = id
+        # Store Coords in List
+        coordsList.append(canvas.coords(id))
+        centerCoordsList.append(((botX+topX) / 2, (botY+topY) / 2))
         # Changes x coordinates
         topX += xOffset; botX += xOffset
     # Changes y coordinates
@@ -80,10 +104,6 @@ for i in range(25):
 for location in bombLocations:
     canvas.itemconfig(location, fill="red") 
 
-#changeButton = tk.Button(root, text="Change Rectangle", command = clickRectangle)
-#changeButton.pack()
 canvas.bind("<Button-1>", rectangleClick)
-
-#canvas.itemconfig(1,fill="green")
 
 root.mainloop()
